b8f6cb0123cba9142cf87e398ed709d9e96c5385
Anuj Mehta <anuj.nm@gmail.com>
2014-12-06 20:13:46 +0000

    Adding CSS to lessons folder

--- lessons/index.php
+++ lessons/index.php
@@ -1,1 +1,70 @@
-Anuj was here
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lesson | The Abstracted Spider</title>
+  <link rel="shortcut icon" type="image/x-icon"/>
+  <link rel="stylesheet" type="text/css" href="">
+
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="http://anujnm.koding.io/xmlrpc.php">
+  <link rel="alternate" type="application/rss+xml" title="The Abstracted Spiders &raquo; Feed" href="http://anujnm.koding.io/feed/" />
+  <link rel="alternate" type="application/rss+xml" title="The Abstracted Spiders &raquo; Comments Feed" href="http://anujnm.koding.io/comments/feed/" />
+  <link rel="alternate" type="application/rss+xml" title="The Abstracted Spiders &raquo; Home Comments Feed" href="http://anujnm.koding.io/home-3/feed/" />
+  <link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='dashicons-css'  href='http://anujnm.koding.io/wp-includes/css/dashicons.min.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='admin-bar-css'  href='http://anujnm.koding.io/wp-includes/css/admin-bar.min.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='contact-form-7-css'  href='http://anujnm.koding.io/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.0.2' type='text/css' media='all' />
+  <link rel='stylesheet' id='thm-style-css'  href='http://anujnm.koding.io/wp-content/themes/starter/style.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='font-awesome-css'  href='http://anujnm.koding.io/wp-content/themes/starter/css/font-awesome.min.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='quick-style-css'  href='http://anujnm.koding.io/wp-content/themes/starter/quick-style.php?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='sportson_preset1-css'  href='http://anujnm.koding.io/wp-content/themes/starter/css/presets/preset1.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='prettyCss-css'  href='http://anujnm.koding.io/wp-content/plugins/themeum-project/css/prettyPhoto.css?ver=4.0.1' type='text/css' media='' />
+  <link rel='stylesheet' id='easyResponsiveTabs-css'  href='http://anujnm.koding.io/wp-content/plugins/fruitful-shortcodes/includes/shortcodes/js/tabs/easy-responsive-tabs.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='fruitful_shortcode_style-css'  href='http://anujnm.koding.io/wp-content/plugins/fruitful-shortcodes/includes/shortcodes/css/fruitful_shortcode_style.css?ver=4.0.1' type='text/css' media='all' />
+  <link rel='stylesheet' id='boostrap-css'  href='http://anujnm.koding.io/wp-content/plugins/fruitful-shortcodes/includes/shortcodes/bootstrap/css/bootstrap.min.css?ver=4.0.1' type='text/css' media='all' />
+  <script type='text/javascript' src='http://anujnm.koding.io/wp-includes/js/jquery/jquery.js?ver=1.11.1'></script>
+  <script type='text/javascript' src='http://anujnm.koding.io/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+  <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://anujnm.koding.io/xmlrpc.php?rsd" />
+  <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://anujnm.koding.io/wp-includes/wlwmanifest.xml" />
+  <meta name="generator" content="WordPress 4.0.1" />
+  <link rel='canonical' href='http://anujnm.koding.io/' />
+  <link rel='shortlink' href='http://anujnm.koding.io/' />
+  <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+  <style type="text/css" media="print">#wpadminbar { display:none; }</style>
+  <style type="text/css" media="screen">
+  html { margin-top: 32px !important; }
+  * html body { margin-top: 32px !important; }
+  @media screen and ( max-width: 782px ) {
+    html { margin-top: 46px !important; }
+    * html body { margin-top: 46px !important; }
+  }
+  </style>
+  
+</head>
+
+<body class="page-id-318 page-template-default logged-in admin-bar  customize-support">
+  <div id="page" class="hfeed site">
+    <header id="masthead" class="site-header" role="banner">
+      <div id="navigation" class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand scroll" href="http://anujnm.koding.io">
+              <span>Lessons<span>
+              </a>
+            </div>
+            <div class="navbar-collapse collapse">
+            </div>
+          </div>
+        </div>
+      </header><!--/#header-->
+
+      Anuj was here
+</body>
+</html>
