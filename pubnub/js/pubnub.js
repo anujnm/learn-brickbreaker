dbf991db8fe49a2fcf5c69b375a0d516d1257dd7
parth_rmehta <parth.rmehta@gmail.com>
2014-12-08 07:05:30 +0530

    With PubNub Integration

---
+++ pubnub/js/pubnub.js
@@ -0,0 +1,59 @@
+﻿var pubnub;
+
+function init_PubNub() {
+    // Initialize the PubNub API connection.
+    var pubnub = PUBNUB.init({
+        publish_key: 'pub-c-bf9c7c26-12d0-44f1-8dd9-f30507609162',
+        subscribe_key: 'sub-c-9824df16-7894-11e4-af64-02ee2ddab7fe'
+    });
+
+    // Grab references for all of our elements.
+    var messageContent = $('#messageContent'),
+        sendMessageButton = $('#sendMessageButton'),
+        messageList = $('#messageList');
+
+    // Handles all the messages coming in from pubnub.subscribe.
+    function handleMessage(message) {
+        var messageEl = $("<li class='message'>"
+            + "<span class='username'>" + message.username + ": </span>"
+            + message.text
+            + "</li>");
+        messageList.append(messageEl);
+        messageList.listview('refresh');
+
+        // Scroll to bottom of page
+        $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 'slow');
+    };
+
+    // Compose and send a message when the user clicks our send message button.
+    sendMessageButton.click(function (event) {
+        var message = messageContent.val();
+
+        if (message != '') {
+            pubnub.publish({
+                channel: 'chat',
+                message: {
+                    username: 'test',
+                    text: message
+                }
+            });
+
+            messageContent.val("");
+        }
+    });
+
+    // Also send a message when the user hits the enter button in the text area.
+    messageContent.bind('keydown', function (event) {
+        if ((event.keyCode || event.charCode) !== 13) return true;
+        sendMessageButton.click();
+        return false;
+    });
+
+    // Subscribe to messages coming in from the channel.
+    pubnub.subscribe({
+        channel: 'chat',
+        message: handleMessage
+    });
+
+
+}
